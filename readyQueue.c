99656054790c55dcfeee1414725a209f4bab24d7
Matthew Seto <matthewseto@D-108-179-159-204.dhcp4.washington.edu>
2015-10-14 15:39:29 -0700

    Began implementing methods

--- readyQueue.c
+++ readyQueue.c
@@ -6,9 +6,10 @@ int main (int argc, char* argv[]){
 	
   int userVal; 
   int i;
+  int PRIORITIES = 64;
 	//make a new array of queues with 64 places
-	Queue priorityArray [64];
-	for(i = 0; i < 64; i++){
+	Queue priorityArray [PRIORITIES];
+	for(i = 0; i < PRIORITIES; i++){
 	  priorityArray[i] = *createQueue();
 	  printf("Created Queue for index %d\n", i);
 	}
@@ -16,7 +17,7 @@ int main (int argc, char* argv[]){
 	//prompt the user for an input
 	  printf("Please enter a priority value for a new process (0-63):\n");
 	  scanf("%d", &userVal);
-	  if(userVal >= 0 && userVal<=63){
+	  if(userVal >= 0 && userVal<=PRIORITIES+1){
 	    //do work to put it in the queue
 	    printf("Successfully added to Ready Queue\n");
 	  } else {
@@ -30,5 +31,59 @@ Queue* createQueue() {
   Q=(Queue *)malloc(sizeof(Queue));
   Q->head = NULL;
   Q->tail = NULL;
+  Q->count = 0;
   return Q;
+}
+
+//enqueue new item to the tail
+//dequeue items from the head
+Queue* enqueue(Queue *myQ, pcb *myPcb){
+	
+	//if the queue is null
+	if(myQ->head == NULL){
+		myQ->head = myPcb;
+	} else {
+		//if not null, add it to the tail
+		myQ->tail->next = myPcb;
+	}
+	
+	myQ->tail = myPcb;
+	//increment the count of nodes
+	myQ->count++;
+}
+
+pcb dequeue(Queue *myQ){
+	
+	//TODO: **** Check for memory leaks later*****
+	
+	//takes the pcb from the head, returns it
+	
+	//make a holder for the head pcb
+	pcb myPcb = myQ->head;
+	//move the head to head.next
+	myQ->head = head->next;
+	//decrement count
+	myQ->count--;
+	//return the pcb 
+	return myPcb;
+	
+}
+
+pcb peek(Queue *myQ){
+	
+	//get the pcb from the head
+	pcb myPcb = myQ->head;
+	//return it
+	return myPcb;
+	
+}
+
+pcb* createPCB(int newID, int newPri, State newState){
+	pcb *myPcb;
+	//allocate memory for it
+	myPcb=(pcb *)malloc(sizeof(pcb));
+	myPcb->id = newID;
+	myPcb->priority = newPri;
+	myPcb->state = newState;
+	return myPcb;	
 }
--- readyQueue.h
+++ readyQueue.h
@@ -9,21 +9,28 @@ typedef struct pcb{
     struct pcb * next;          //next pcb in the queue/linked list
 } pcb;
 
-typedef pcb * pcb_p; 
+//typedef pcb * pcb_p; 
 
 
 typedef struct Queue {
     int count;              	//count of elements
-	struct node *head;			//head of the queue
-	struct node *tail;			//tail of the queue
+	struct pcb *head;			//head of the queue
+	struct pcb *tail;			//tail of the queue
+	
+	//methods declarations
+	void (*enqueue)(struct Queue*, struct pcb*);
+	pcb (*dequeue)(struct Queue*);
+	pcb (*peek)(struct Queue*);
+	
 } Queue;
 
 //declare method to create new PCB
 pcb* createPCB(int, int, State);
 
-// declare methods for the queue
+// method prototypes
 Queue* createQueue();
-Queue* peek();
-Queue* enqueue();
-Queue* dequeue();
+Queue* peek(Queue *myQ);
+Queue* enqueue(Queue *myQ, pcb *myPcb);
+Queue* dequeue(Queue *myQ);
+
 
